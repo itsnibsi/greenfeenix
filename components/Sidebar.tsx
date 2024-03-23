0ce0ee7b684b6a4082e1f59091e38e949fddf789
Nico Hämäläinen <nibsivaan@gmail.com>
2024-03-23 14:50:55 +0200

    sidebar links
    Signed-off-by: Nico Hämäläinen <nibsivaan@gmail.com>

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,8 +10,8 @@ const Sidebar = () => {
   return (
     <div className="flex flex-col p-4">
       <Link href="/" className="md:-mt-8 -mt-0"><Logo /></Link>
-      <nav className="flex flex-col items-start gap-4 text-lg [&>a.active]:bg-green-500 [&>a.active]:text-white [&>a]:px-3 [&>a]:py-1 [&>a.active]:font-semibold [&>a:not(.active):hover]:bg-orange-200">
-        <Link href="/#content" className={`link ${pathname === '/' ? 'active' : ''}`}>Home</Link>
+      <nav className="flex flex-col items-start gap-4 text-lg [&>a.active]:bg-green-500 [&>a.active]:text-white [&>a]:px-3 [&>a]:py-1 [&>a.active]:font-semibold [&>a:not(.active)]:border-b-2 [&>a]:border-white [&>a:not(.active):hover]:border-orange-200">
+        <Link href="/" className={`link ${pathname === '/' ? 'active' : ''}`}>Home</Link>
         <Link href="/travel#content" className={`link ${pathname === '/travel' ? 'active' : ''}`}>Travel Businesses</Link>
         <Link href="/construction-real-estate#content" className={`link ${pathname === '/construction-real-estate' ? 'active' : ''}`}>Construction Businesses</Link>
         <Link href="/cities-communities#content" className={`link ${pathname === '/cities-communities' ? 'active' : ''}`}>Cities & Communities</Link>
