adb149a03dff76e75be4daa9bdf8bf4088544a8a
Nico Hämäläinen <nibsivaan@gmail.com>
2024-03-24 13:13:55 +0200

    switch up styles
    Signed-off-by: Nico Hämäläinen <nibsivaan@gmail.com>

--- components/ContactCallout.tsx
+++ components/ContactCallout.tsx
@@ -4,7 +4,7 @@ const ContactCallout = () => (
   <div className="text-lg">
     <a
       href="/contact"
-      className="mb-2 mt-4 block border-t-2 border-gray-100 pt-4 text-green-600 transition-colors duration-200 hover:text-orange-500"
+      className="mb-2 mt-8 block border-t-2 border-gray-100 pt-8 text-green-600 transition-colors duration-200 hover:text-orange-500"
     >
       <PaperAirplaneIcon className="mr-2 inline-block h-6 w-6" />
       Contact us for more information
